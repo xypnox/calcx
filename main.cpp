a74c87133e942a39d419f1408eae429e6bf2d810
AdiUltra <adityaultra4@gmail.com>
2016-10-02 23:52:27 +0530

    At least now the calculator works

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+
 #include <iostream>
 #include <cstring>
+
+#define as_string( s ) # s
+
 
 using namespace std;
 
@@ -11,6 +15,8 @@ using namespace std;
     Commented lines are nice for debugging
 
  */
+
+double ANSWER=0.0;
 
 double convertDouble(char a[]) {
     // This Fxn Converts a char to double
@@ -43,7 +49,6 @@ void strip(char a[]) {
         a[i] = a[i+c];
         // std::cout << a << std::endl;
     }
-    // std::cout << c << std::endl;
     a[i++] = '\0';
 }
 
@@ -55,62 +60,154 @@ int checkValid(char t) {
     }
 }
 
+double operate(double a1, double a2, char op) {
+    switch (op) {
+        case '+': return a1+a2; break;
+        case '-': return a1-a2; break;
+        case '*': return a1*a2; break;
+        case '/': return a1/a2; break;
+        default: return -101;
+    }
 
-struct element {
+}
+
+
+class element {
+public:
     char op;
     double value;
     char type;
     element *next;
+    element *prev;
+
+    void display() {
+        if (type == 'o') {
+            std::cout << op << " ";
+        } else {
+            std::cout << value << " ";
+        }
+    }
 };
 
 class MultiStack {
     element *start;
+    element *last;
+    int length;
 public:
     MultiStack() {
         start = NULL;
+        length = 0;
     }
     void push(element k) {
         element *nn = new element;
-        nn -> op = k.op;
-        nn -> value = k.value;
+        if (k.type == 'o') {
+            nn -> op = k.op;
+        } else {
+            nn -> value = k.value;
+        }
         nn -> type = k.type;
         nn -> next = NULL;
+        nn -> prev = NULL;
         if (start == NULL) {
-            start = nn;
+            start = last = nn;
         } else {
+            start -> prev = nn;
             nn -> next = start;
             start = nn;
         }
+        length++;
     }
 
-    element pop() {
-        // reurns the topmos operator after deleting it from stack
+    char popOp(char pos='f') {
+        // returns the topmost operator after deleting it from stack
+        char ret='E';
+        if (length != 0) {
+
+            if (start == last) {
+                ret = start->op;
+                start = last = NULL;
+                length--;
+            } else if (pos == 'f') {
+                ret = start->op;
+                element *t = start->next, *y = start;
+                start = t;
+                delete y;
+                t->prev = NULL;
+                length--;
+            } else if (pos == 'l') {
+                ret = last->op;
+                element *x = last;
+                x->prev->next = NULL;
+                last = last->prev;
+                delete x;
+                length--;
+            }
+        }
+
+        return ret;
+
+    }
+
+    double popNo(char pos='l') {
+        // returns the topmost operator after deleting it from stack
+        double ret = -101;
+        if (start == last) {
+            ret = start->value;
+            start = last = NULL;
+            length--;
+        } else if (length != 0 && last->type=='n') {
+            ret = last->value;
+            element *x = last;
+            x->prev->next = NULL;
+            last = last->prev;
+            delete x;
+            length--;
+        }
+        return ret;
+    }
+
+    char topType() {
         if (start != NULL) {
-            element x = *start;
-            start = start -> next;
-            return x;
+            return start->type;
+        } else {
+            return 'E';
         }
     }
 
-    element top() {
+    char lastType() {
+        if (last != NULL) {
+            return last->type;
+        } else {
+            return 'E';
+        }
+    }
+
+    char topOp() {
         if (start != NULL) {
-            element n = *start;
-            return n;
+            return start->op;
+        } else {
+            return 'E';
+        }
+    }
+
+    double topNo() {
+        if (start != NULL) {
+            return start->value;
+        } else {
+            return -101;
         }
     }
 
     void display() {
         element *t = start;
         while(t != NULL) {
-            if (t->type == 'o') {
-                std::cout << t->op << " ";
-            } else if (t->type == 'n') {
-                std::cout << t->value << " ";
-            }
+            t->display();
             t = t->next;
         }
         std::cout << std::endl;
     }
+
+    int len() { return length; }
 };
 
 void parse(char a[], MultiStack &stack, MultiStack &pfx) {
@@ -121,7 +218,6 @@ void parse(char a[], MultiStack &stack, MultiStack &pfx) {
     for (size_t i = 0; i < strlen(a); i++) {
 
         if ((a[i] < 58 && a[i] > 47) || a[i] == 46) {
-            std::cout << "found number" << std::endl;
             num[k++] = a[i];
 
             if ((i == strlen(a)-1) || checkValid(a[i+1])) {
@@ -133,33 +229,95 @@ void parse(char a[], MultiStack &stack, MultiStack &pfx) {
                 tmp.value = convertDouble(num);
 
                 pfx.push(tmp);
-                // pfx.display();
             }
         } else if (checkValid(a[i])) {
-            // std::cout << "Hi" << std::endl;
-            element tmp;
-            tmp.type = 'o';
+            element tmp, emp;
+            tmp.type = emp.type = 'o';
             tmp.op = a[i];
 
+            if ((tmp.op == '-' || tmp.op == '+') && (stack.topOp() == '*' || stack.topOp() == '/')) {
+                emp.op = stack.popOp();
+                while (emp.op != 'E' || stack.topOp() == '*' || stack.topOp() == '/' ) {
+                    pfx.push(emp);
+                    emp.op = stack.popOp();
+                }
+            }
             stack.push(tmp);
-            // stack.display();
         }
     }
+    element j;
+    j.op = stack.popOp();
+    j.type = 'o';
+    while (j.op != 'E') {
+        pfx.push(j);
+        j.op = stack.popOp();
+    }
 }
+
+double evaluate(MultiStack &pfx) {
+    MultiStack calc;
+    double ret=-101;
+    while (pfx.len() != 0) {
+
+        if (pfx.lastType() == 'n') {
+            element tmp;
+            tmp.value = pfx.popNo();
+            tmp.type = 'n';
+            calc.push(tmp);
+        } else if (pfx.lastType() == 'o') {
+            double a, b;
+            char op = pfx.popOp('l');
+            if (calc.lastType() == 'n') {
+                a = calc.popNo();
+                if (calc.lastType() == 'n') {
+                    b = calc.popNo();
+                } else {
+                    std::cout << "ERROR : Syntax" << std::endl;
+                    ret = -101;
+                    break;
+                }
+            } else {
+                std::cout << "ERROR : Syntax" << std::endl;
+                ret = -101;
+                break;
+            }
+            element tmp;
+            tmp.value = operate(a, b, op);
+            tmp.type = 'n';
+            calc.push(tmp);
+            ret = calc.topNo();
+        }
+
+    }
+
+    return ret;
+}
+
 
 int main() {
     MultiStack stack, pfx;
 
     char x[80];
-    std::cout << " >> ";
-    cin.getline(x, 80);
+    while (1) {
+        if (!strcmp(x, "quit")*strcmp(x, "exit")) {
+            break;
+        } else {
+            std::cout << " >> ";
+            cin.getline(x, 80);
 
-    parse(x, stack, pfx);
+            parse(x, stack, pfx);
 
-    pfx.display();
-    stack.display();
+            double k = evaluate(pfx);
+            if (k != -101) {
+                std::cout << k << std::endl;
+            }
+        }
+    }
 
 
+
+    // pfx.display();
+    // stack.display();
     // char a[90] = "x + y + u 98 . 88";
     // std::cout << a << std::endl;
     // strip(a);
